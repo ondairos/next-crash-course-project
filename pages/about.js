3c31d2e5e0e1be8c2d1925bba1dea0c984a67513
Ondairos <ondairos@gmail.com>
2022-10-16 16:46:49 +0300

    about fix

--- pages/about.js
+++ pages/about.js
@@ -3,8 +3,9 @@ import Meta from '../components/Meta'
 const about = () => {
     return (
         <div>
-            <Meta title='About'/>
+            <Meta title='About' />
             <h1>About Page</h1>
+
         </div>
     )
 }
