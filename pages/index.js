9c72dbbfd5ad055e6df8754ed63f6e67c32268c6
Ondairos <ondairos@gmail.com>
2022-10-16 14:35:48 +0300

    added inital Article module

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>Web News</title>
-        <meta name='keywords' content='web development, programming'/>
+        <meta name='keywords' content='web development, programming' />
       </Head>
 
-      <h1> Welcome to next</h1>
+      {articles.map(article => (
+        <h3>{article.title}</h3>
+      ))}
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
