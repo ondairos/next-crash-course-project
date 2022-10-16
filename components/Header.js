e5bba3338c7c7bebe155601d5ae0f29d0d154802
Ondairos <ondairos@gmail.com>
2022-10-16 14:18:56 +0300

    refactored header module

--- components/Header.js
+++ components/Header.js
@@ -3,16 +3,10 @@ import headerStyles from '../styles/Header.module.css'
 const Header = () => {
     return (
         <div>
-            <h1 className='title'>
+            <h1 className={headerStyles.title}>
                 <span>WebDev</span>News
             </h1>
-            <style jsx>
-                {`
-                    .title {
-                    color: red;
-                    }
-                `}
-            </style>
+            <p className={headerStyles.description}>Keeping up to date with all the latest dev news!</p>
         </div>
     )
 }
